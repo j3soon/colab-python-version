db4111fcf5f9c8062475b156927dd64e311013d3
Johnson Sun <j3.soon777@gmail.com>
2025-04-21 23:21:10 +0800

    Add python 3.9 script

---
+++ scripts/py39.sh
@@ -0,0 +1,9 @@
+# This script is intended to be used with a specially modified Colab notebook.
+# i.e., https://colab.research.google.com/github/j3soon/colab-python-version/blob/main/notebooks/py310.ipynb
+# For more information, see: https://github.com/j3soon/colab-python-version.
+
+wget -O miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-py39_25.1.1-2-Linux-x86_64.sh
+bash ./miniconda.sh -b -f -p /usr/local
+conda install -q -y jupyter google-colab traitlets=5.5.0 -c conda-forge
+python -m ipykernel install --name "py39" --user
+rm ./miniconda.sh
